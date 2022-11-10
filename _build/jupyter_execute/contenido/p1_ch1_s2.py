bc9f13892d1e783d27b5c086cd3b102fd8c05b60
tigarto <helpu1409@gmail.com>
2022-11-09 23:50:31 -0500

    Actualizando notebook metricas

--- _build/jupyter_execute/contenido/p1_ch1_s2.py
+++ _build/jupyter_execute/contenido/p1_ch1_s2.py
@@ -270,7 +270,7 @@ from scipy.stats import trim_mean
 
 # #### Solución usando código Python
 
-# In[7]:
+# In[31]:
 
 
 # Implementacion de la media
@@ -286,7 +286,7 @@ print("Promedio de las alturas (cm):", mean_height)
 
 # #### Solución usando numpy
 
-# In[8]:
+# In[32]:
 
 
 # Solucion numpy
@@ -299,7 +299,7 @@ print("Altura promedio (cm): {0:.2f}".format(mean_height))
 
 # #### Solución usando pandas
 
-# In[15]:
+# In[33]:
 
 
 # Solucion pandas
@@ -308,4 +308,204 @@ df_heights= pd.DataFrame(data_heights)
 print(df_heights)
 mean_height = df_heights.mean()
 print("Altura promedio: {0:.2f}".format(mean_height[0]))
+
+
+# ### Ejemplo 2
+# 
+# Dado los los siguientes datos: 22, 25, 29, 11, 14, 18, 13, 13, 17, 11, 8, 8, 7, 12, 15, 6, 8, 7, 9, 12. Calcule la media recortando un 10% de los datos.
+
+# In[34]:
+
+
+# Se uso la funcion trim_mean de scipy.stats
+
+data = [22, 25, 29, 11, 14, 18, 13, 13, 17, 11, 8, 8, 7, 12, 15, 6, 8, 7, 9, 12]
+
+#calculate 10% trimmed mean
+print("Datos:",data)
+print("Media 10% truncada: ",trim_mean(data, 0.1), sep = "")
+
+
+# ### Ejemplo 3
+# Asuma que la altura (en cm) de los estudiantes de una clase es como sigue: 90,102,110,115,85,90,100,110,110. ¿Cual es la media de alturas?
+
+# #### Solución usando numpy
+
+# In[35]:
+
+
+heights =  np.array([90,102,110,115,85,90,100,110,110])
+print("heights:",heights)
+print("heights ordenadas:",np.sort(heights))
+
+median_height = np.median(heights)
+print("Mediana: {0:.2f}".format(median_height))
+
+
+# #### Solución usando pandas
+
+# In[36]:
+
+
+# Solucion pandas
+data_heights = {'heights':pd.Series([90,102,110,115,85,90,100,110,110])}
+df_heights= pd.DataFrame(data_heights)
+print(df_heights)
+median_height = df_heights.median()
+print("Altura promedio: {0:.2f}".format(median_height[0]))
+
+
+# ### Ejemplo 4
+# Asuma que la altura (en cm) de los estudiantes de una clase es como sigue: 90,102,110,115,85,90,100,110,110. ¿Cual es el rango de las alturas?
+
+# #### Solución usando numpy
+
+# In[37]:
+
+
+# Solucion numpy
+heights =  np.array([90,102,110,115,85,90,100,110,110])
+print("heights:",heights)
+min_height = np.min(heights)
+max_height = np.max(heights)
+range_height = max_height - min_height
+print("Rango [{0:d},{1:d}]: {2:d}".format(min_height, max_height, range_height))
+
+
+# #### Solución usando pandas
+
+# In[38]:
+
+
+# Solucion pandas
+data_heights = {'heights':pd.Series([90,102,110,115,85,90,100,110,110])}
+df_heights= pd.DataFrame(data_heights)
+print(df_heights)
+min_height = df_heights.min()
+max_height = df_heights.max()
+range_height = max_height[0] - min_height[0]
+print("Rango [{0:d},{1:d}]: {2:d}".format(min_height[0], max_height[0], range_height))
+
+
+# ### Ejemplo 5
+# 
+# El sitio web www.fueleconomy.gov contiene una gran cantidad de información acerca de las características del combustible de varios vehículos. Además de las calificaciones de millaje de la EPA, hay muchos vehículos para los que los usuarios han informado de sus propios valores de eficiencia de combustible (mpg). Considere la siguiente muestra de $n = 11$ eficiencias para el Ford Focus 2009 equipado con transmisión automática (para este modelo, la EPA informa de una calificación general de 27 mpg-24 mpg en ciudad y 33 mpg en carretera) las cuales se muestran a continuación:
+# 
+# 
+# |$x_i$|
+# |---|
+# |27.3|
+# |27.9|
+# |32.9|
+# |35.2|
+# |44.9|
+# |39.9|
+# |30.0|
+# |29.7|
+# |28.5|
+# |32.0|
+#     
+# Obtenga:
+# 1. La media.
+# 2. Las desviaciones.
+# 3. Las desviaciones al cuadrado
+# 4. La desviacion estandar
+# 5. La varianza
+
+# #### Solución usando numpy
+
+# In[39]:
+
+
+# Solucion numpy
+# Calculos empleando funciones de acuerdo a las formulas
+epa_data =  np.array([27.3, 27.9, 32.9, 35.2, 44.9, 39.9, 30.0, 29.7, 28.5, 32.0, 37.6])
+epa_mean = np.mean(epa_data)
+epa_desv = epa_data - np.array(epa_data.size*[epa_mean])
+epa_ec = epa_desv**2
+epa_var = np.sum(epa_ec)/(epa_data.size - 1)
+epa_std = (epa_var)**0.5
+# Despliegue de valores
+print("RESUMEN DE LAS MEDIDAS DE DISPERSION")
+print("-----------------------------------------------------------")
+print("EPA (mpg): ",epa_data)
+print("EPA Media: ",epa_mean)
+print("Desviaciones EPA: ",epa_desv)
+print("Error cuadratico EPA: ",epa_ec)
+print("Varianza: ",epa_var)
+print("Desviacion estandar EPA: ",epa_std)
+print("-----------------------------------------------------------")
+# Obtencion de la media, la desviacion estandar y la varianza usando las formulas de numpy
+epa_mean = np.mean(epa_data)
+epa_std = np.std(epa_data, ddof=1)
+epa_var = np.var(epa_data, ddof=1)
+print("EPA Media: ",epa_mean)
+print("Varianza EPA [np.std]: ",epa_var)
+print("Desviacion estandar [np.var]: ",epa_std)
+
+
+# #### Solución usando pandas
+
+# In[40]:
+
+
+# Solución pandas
+data = {'epa_data':pd.Series([27.3, 27.9, 32.9, 35.2, 44.9, 39.9, 30.0, 29.7, 28.5, 32.0, 37.6])}
+epa_data = pd.DataFrame(data)
+epa_mean = epa_data.mean()
+epa_desv = epa_data - epa_mean
+epa_desv = epa_desv.rename(columns={'epa_data':'epa_dist'})
+epa_ec = epa_desv**2
+epa_ec = epa_ec.rename(columns={'epa_data':'epa_ec'})
+epa_var =  epa_data.var()
+epa_std = epa_data.std()
+df_summary = pd.concat([epa_data,epa_desv,epa_ec],axis=1)
+df_summary
+
+
+# In[41]:
+
+
+print("EPA Media: ",epa_mean)
+print("Varianza EPA: ",epa_var)
+print("Desviacion estandar: ",epa_std)
+
+
+# ### Ejemplo 6
+# 
+# Se utilizó ultrasonido para reunir los datos adjuntos de corrosión en el espesor de la placa de piso de un tanque elevado utilizado para almacenar petróleo crudo (“Statistical Analysis of UT Corrosion Data from Floor Plates of a Crude Oil Aboveground Storage Tank”, Materials Eval., 1994: 846–849); cada observación es la profundidad de la picadura más grande en la placa, expresada en milésimas de pulgada: 40, 52, 55, 60, 70, 75, 85, 85, 90, 90, 92, 94, 94, 95, 98, 100, 115, 125, 125.
+# 
+# Muestre el resumen de los cinco numeros
+
+# #### Solución usando numpy
+
+# In[42]:
+
+
+# Usando numpy
+profundidad = np.array([40, 52, 55, 60, 70, 75, 85, 85, 90, 90, 92, 94, 94, 95, 98, 100, 115, 125, 125])
+min_prof = np.quantile(profundidad,q = 0.0) # np.percentile(profundidad,q = 0)
+max_prof = np.quantile(profundidad,q = 1.0) # np.percentile(profundidad,q = 100)
+Q1_prof = np.quantile(profundidad,q = 0.25) # np.percentile(profundidad,q = 25)
+Q3_prof = np.quantile(profundidad,q = 0.75) # np.percentile(profundidad,q = 75)
+median_prof = np.quantile(profundidad,q = 0.5) # np.median(profundidad)
+rango_prof = np.ptp(profundidad)
+print("Resumen profundidades")
+print("- Minimo: ", min_prof, sep="")
+print("- Q1: ", Q1_prof , sep="")
+print("- Mediana: ", median_prof , sep="")
+print("- Q3: ", Q3_prof , sep="")
+print("- Maximo: ", max_prof, sep="")
+print("- IRQ: ", Q3_prof - Q1_prof, sep="")
+
+
+# #### Solución usando Pandas
+
+# In[43]:
+
+
+# Solucion pandas
+data = {'epa_data':pd.Series([27.3, 27.9, 32.9, 35.2, 44.9, 39.9, 30.0, 29.7, 28.5, 32.0, 37.6])}
+epa_data = pd.DataFrame(data)
+epa_data.describe()
 
