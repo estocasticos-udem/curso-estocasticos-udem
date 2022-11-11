728aa07006053409fbee711f075da202d8655881
tigarto <helpu1409@gmail.com>
2022-11-11 00:00:09 -0500

    Agregando ejemplos de histogramas

--- _build/jupyter_execute/contenido/p1_ch1_s3.py
+++ _build/jupyter_execute/contenido/p1_ch1_s3.py
@@ -26,7 +26,9 @@ import seaborn as sns
 #  
 # En primer lugar, se determinan la *frecuencia* y la *frecuencia* relativa de cada valor $x_i$. Luego se marcan los valores $x_i$ posibles en una escala horizontal. Sobre cada valor se traza un rectángulo cuya altura es la frecuencia relativa (o alternativamente, la frecuencia) de dicho valor: Los rectángulos deben medir lo mismo de ancho.
 # 
-# **Ejemplo**: ¿Qué tan inusual es un juego de beisbol sin [hit](https://es.wikipedia.org/wiki/Hit_(b%C3%A9isbol)) o de un hit en las ligas mayores y cuán frecuentemente un equipo pega más de 10, 15 o incluso 20 hits? La siguiente tabla muestra el numero de hits conectados por equipo y por juego de todos los juegos de nueve episodios que se jugaron entre 1989 y 1993. (**Nota**: Este ejemplo fue tomado del libro **Probabilidad y estadistica para Ciencias e Ingenieria** (Jay L. Devore)).
+# #### Ejemplo 1
+# 
+# ¿Qué tan inusual es un juego de beisbol sin [hit](https://es.wikipedia.org/wiki/Hit_(b%C3%A9isbol)) o de un hit en las ligas mayores y cuán frecuentemente un equipo pega más de 10, 15 o incluso 20 hits? La siguiente tabla muestra el numero de hits conectados por equipo y por juego de todos los juegos de nueve episodios que se jugaron entre 1989 y 1993. (**Nota**: Este ejemplo fue tomado del libro **Probabilidad y estadistica para Ciencias e Ingenieria** (Jay L. Devore)).
 # 
 # |Hits/juego|Numero de juegos|
 # |---|---|
@@ -64,7 +66,7 @@ import seaborn as sns
 # 2. Calcule la frecuencia relativa.
 # 3. Dibuje el histograma.
 
-# In[2]:
+# In[40]:
 
 
 # 1
@@ -75,16 +77,16 @@ raw_data = {"hits_per_game":np.arange(28),
            }
 
 df = pd.DataFrame(raw_data, columns = raw_data.keys())
-df
+df.head()
 
 
-# In[4]:
+# In[41]:
 
 
 # 2
 df_freq = df.copy()
 df_freq['freq_rel']= df['games']/df['games'].sum()
-df_freq
+df_freq.head()
 
 
 # In[6]:
@@ -94,17 +96,22 @@ print("Catidad total de juegos", df_freq['games'].sum())
 print("Suma de las frecuencias", df_freq['freq_rel'].sum())
 
 
-# In[12]:
+# In[38]:
 
 
 # Ya se han contado los elementos (se uso pandas para el histograma) 
-df_freq['freq_rel'].plot(kind='bar', title ='Histograma del número de hits por juego de nueve entradas');
+df_freq['freq_rel'].plot(kind='bar', 
+                         title ='Histograma del número de hits por juego de nueve entradas',
+                         xlabel="Hits/Juego",
+                         ylabel="Frecuencia relativa");
 
 
 # ### Histograma a partir de datos continuos
 # En este caso, se divide el eje de medición entre un número adecuado de **intervalos de clase o clases** (**bins**). Cuando el ancho de las clases es el mismo, lo que se hace es determinar la frecuencia y la frecuencia relativa de cada clase contando la cantidad de muestras que se encuentran dentro de cada intervalo y usando frecuencia relativa como la altura del correspondiente intervalo.
 # 
-# **Ejemplo**: Los siguientes datos muestran la cantidad de horas dedicadas a jugar juegos de video de un grupo de estudiantes.
+# #### Ejemplo 2
+# 
+# Los siguientes datos muestran la cantidad de horas dedicadas a jugar juegos de video de un grupo de estudiantes.
 # 
 # ```
 # 9.95  10  2.25  16.75  0
@@ -116,9 +123,9 @@ df_freq['freq_rel'].plot(kind='bar', title ='Histograma del número de hits por 
 # 
 # Construya un histograma agrupando los datos en cinco clases.
 # 
-# #### Solución empleando Matplotlib
+# ##### Grafica empleando Matplotlib
 
-# In[13]:
+# In[33]:
 
 
 # --- Usando Numpy ---
@@ -129,56 +136,169 @@ data = np.array([9.95, 10, 2.25, 16.75, 0, 19.5, 22.5, 7.5, 15, 12.75,
 data
 
 
-# In[14]:
+# In[70]:
 
 
 # Histograma de cinco grupos
+plt.style.use('classic')
 hist = np.histogram(data, bins=5)
 hist
 
 
-# In[32]:
+# In[37]:
 
 
 # Grafica del histograma usando Matplotlib
 fig, ax = plt.subplots()
-#
-ax.grid(True)
+
 ax.hist(data, bins=5,histtype='bar', rwidth=0.5)
-ax.set_title('Histograma'); # Nota: Es necesario dar mas contexto al histograma
+ax.set_xlabel("Horas de juego");
+ax.set_ylabel("Numero de estudiantes");
+ax.set_title('Histograma'); 
 
 
-# In[ ]:
+# ##### Grafica empleando Pandas
+
+# In[49]:
 
 
-from matplotlib import rcParams, cycler
-import matplotlib.pyplot as plt
-import numpy as np
-plt.ion()
+# Creacion del dataframe
+data = np.array([9.95, 10, 2.25, 16.75, 0, 19.5, 22.5, 7.5, 15, 12.75,
+        5.5, 11, 10, 20.75, 17.5, 23, 21.9, 24, 23.75, 18,
+        20, 15, 22.9, 18.8, 20.5])
+
+df = pd.DataFrame({'hours': data})
+df.head() 
 
 
-# In[ ]:
+# In[50]:
 
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-N = 10
-data = [np.logspace(0, 1, 100) + np.random.randn(100) + ii for ii in range(N)]
-data = np.array(data).T
-cmap = plt.cm.coolwarm
-rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, N)))
+hist = pd.cut(df['hours'], bins = 5).value_counts().sort_values(ascending=True)
+hist
 
 
-from matplotlib.lines import Line2D
-custom_lines = [Line2D([0], [0], color=cmap(0.), lw=4),
-                Line2D([0], [0], color=cmap(.5), lw=4),
-                Line2D([0], [0], color=cmap(1.), lw=4)]
-
-fig, ax = plt.subplots(figsize=(10, 5))
-lines = ax.plot(data)
-ax.legend(custom_lines, ['Cold', 'Medium', 'Hot']);
+# In[64]:
 
 
-# There is a lot more that you can do with outputs (such as including interactive outputs)
-# with your book. For more information about this, see [the Jupyter Book documentation](https://jupyterbook.org)
+plt.style.use('ggplot')
+hist.plot(kind='bar',xlabel="Horas de juego", ylabel="Numero de estudiantes",title="Histograma");
+
+
+# ##### Grafica empleando Seaborn
+
+# In[65]:
+
+
+plt.style.use('seaborn-darkgrid')
+sns.histplot(df,
+             bins=5,
+             x="hours",
+             kde=True,
+             shrink=.5);
+
+
+# #### Ejemplo 3
+# 
+# El artículo **"Determination of Most Representative Subdivision"** (J. of Energy Engr., 1993: 43-55) proporciona datos sobre varias características de subdivisiones que podrían utilizarse para decidir si se suministra energía eléctrica mediante líneas elevadas o por medio de líneas subterráneas. He aquí los valores de la variable *x = longitud total de calles dentro de una subdivisión*:
+# 
+# ```
+# 1280 5320 4390 2100 1240 3060 4770
+# 1050 360 3330 3380 340 1000 960
+# 1320 530 3350 540 3870 1250 2400
+# 960 1120 2120 450 2250 2320 2400
+# 3150 5700 5220 500 1850 2460 5850
+# 2700 2730 1670 100 5770 3150 1890
+# 510 240 396 1419 2109
+# ```
+# 
+# Construya un histograma con los límites de clase, 0, 1000, 2000, 3000, 4000, 5000 y 6000.
+
+# ##### Solución empleando Matplolib
+
+# In[71]:
+
+
+# Usando Numpy
+# Creacion de la tabla
+data = [1280, 5320, 4390, 2100, 1240, 3060, 4770, 1050, 360, 3330, 3380, 340, 1000, 960,
+        1320, 530, 3350, 540, 3870, 1250, 2400, 960, 1120, 2120, 450, 2250, 2320, 2400, 
+        3150, 5700, 5220, 500, 1850, 2460, 5850, 2700, 2730, 1670, 100, 5770, 3150, 1890,
+        510, 240, 396, 1419, 2109]
+print(data)
+
+
+# In[72]:
+
+
+hist = np.histogram(data, bins=6, range=(0,6000))
+hist
+
+
+# In[83]:
+
+
+fig, ax = plt.subplots()
+ax.hist(data, bins=6, range=(0,6000), rwidth = 0.5);
+ax.set_xlabel("Longitud de las calles");
+ax.set_ylabel("Numero de calles");
+plt.title('Histograma');
+#plt.title('Histograma de longitud total de calles dentro de una subdivisión')
+#plt.grid(True)
+#plt.hist(data, bins=6, range=(0,6000));
+
+
+# ##### Solución empleando Pandas
+
+# In[84]:
+
+
+df = pd.DataFrame({'long_calles':[1280, 5320, 4390, 2100, 1240, 3060, 4770, 1050, 360, 3330, 3380, 340, 1000, 960,
+        1320, 530, 3350, 540, 3870, 1250, 2400, 960, 1120, 2120, 450, 2250, 2320, 2400, 
+        3150, 5700, 5220, 500, 1850, 2460, 5850, 2700, 2730, 1670, 100, 5770, 3150, 1890,
+        510, 240, 396, 1419, 2109]})
+df.head()
+
+
+# In[87]:
+
+
+clases = pd.IntervalIndex.from_tuples([(0, 1000), (1000, 2000) ,(2000, 3000), (3000, 4000), (4000, 5000), (5000, 6000)])
+hist = pd.cut(df['long_calles'], bins = clases)
+hist
+
+
+# In[88]:
+
+
+freq_rel = hist.value_counts()
+freq_rel
+
+
+# In[89]:
+
+
+df_hist = pd.DataFrame({'longitudes' : ['[0, 1000]','(1000, 2000]','(2000, 3000]','(3000, 4000]','(4000, 5000]','(5000, 6000]'],
+                        'freq_relativa': freq_rel})
+df_hist
+
+
+# In[91]:
+
+
+df_hist.plot(kind='bar', 
+             title ='Histograma de longitud total de calles dentro de una subdivisión',
+             x="longitudes");
+
+
+# ##### Solución empleando Seaborn
+
+# In[101]:
+
+
+ax = sns.histplot(data,
+             bins=[0,1000,2000,3000,4000,5000,6000],             
+             kde=True,
+             shrink=.8);
+ax.set(xlabel='Longitudes');
+
