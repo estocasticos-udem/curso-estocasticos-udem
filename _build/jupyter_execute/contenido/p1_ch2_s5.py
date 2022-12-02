3738d7f9525c398308a490431556bb15b57a4688
tigarto <helpu1409@gmail.com>
2022-12-02 00:18:44 -0500

    actualizando

--- _build/jupyter_execute/contenido/p1_ch2_s5.py
+++ _build/jupyter_execute/contenido/p1_ch2_s5.py
@@ -43,46 +43,143 @@ def prob(num_events, total):
 # * $M$ = es hombre
 # 
 # Se pide: 
-# 1. Encuentre $P(B|M)$ y explique lo que significa.
-# 2. Encuentre $P(M|B)$ y explique lo que significa.
-# 3. ¿Todos los eventos $B$ y $M$ son mutuamente excluyentes?
-# 4. ¿Todos los eventos $B$ y $M$ son independientes?
+# 1. Encuentre la probabilidad de que se elija una mujer.
+# 2. Encuentre la probabilidad de que la persona elegida desayune regularmente.
+# 3. Encuentre la probabilidad de que la persona elegida sea una mujer que desayune regularmente.
+# 4. Encuentre la probabilidad de que la persona elegida sea una mujer o cualquiera que desayune regularmente.
+# 5. Encuentre $P(B \bigcup M)$ y explique lo que significa.
+# 6. Encuentre $P(B^C \bigcup M^C)$ y explique lo que significa.
+# 7. Encuentre $P(B|M)$ y explique lo que significa.
+# 8. Encuentre $P(M|B)$ y explique lo que significa.
+# 9. ¿Todos los eventos $B$ y $M$ son mutuamente excluyentes?
+# 10. ¿Todos los eventos $B$ y $M$ son independientes?
 
 # In[3]:
 
 
-
-regular_breakdast = np.array([1, 0], dtype=object)
-man = np.array([190, 110], dtype=object)
-woman = np.array([110, 165], dtype=object)
-
 data = {
-  "regular_breakdast": np.array([1, 0]),
-  "man": np.array([190, 110]),
-  "woman": np.array([130, 165])
+  "regular_breakdast": np.array(["si", "no"], dtype="str"),
+  "man": np.array([190, 130]),
+  "woman": np.array([110, 165])
 }
 
-cont_tab = pd.DataFrame(data)
-print(cont_tab)
+tabla = pd.DataFrame(data)
+tabla
 
-total_sex = cont_tab[['man','woman']].sum(axis = 0) # Suma columnas
-print(total_sex)
-total_breakfast = cont_tab[['man','woman']].sum(axis = 1) # Suma Filas
-print(total_breakfast)
+
+# In[4]:
+
+
+print("--- Cantidad total de \"hombres\" y \"mujeres\" --- ")
+total_sex = tabla[['man','woman']].sum(axis = 0) # Suma columnas
+total_sex
+
+
+# In[5]:
+
+
+print("--- Cantidad total de personas que \"no desayuna\" y \"desayuna\" regularmente --- ")
+total_breakfast = tabla.sum(axis = 1, numeric_only=True) # Suma Filas
+total_breakfast
+
+
+# In[6]:
+
+
 # 1
-total_break =  total_breakfast[0]
-num_break_and_woman = cont_tab.iloc[0]['woman']
-P_B_dado_M = prob(num_break_and_woman,total_break)
-print(P_B_dado_M)
+print("Punto 1 ->")
+N_woman =  total_sex['woman']
+N = total_sex.sum() 
+P_notM = prob(N_woman,N)
+print(f"P(M') = {P_notM:.4f}")
+
+
+# In[7]:
+
 
 # 2
-num_break_and_woman =  cont_tab.iloc[0]['woman']
-total_woman = total_sex[1]
-P_M_dado_B = prob(num_break_and_woman,total_woman)
-print(P_M_dado_B)
+print("Punto 2 ->")
+N_break =  total_breakfast[0]
+N = total_breakfast.sum()
+P_B = prob(N_break,N)
+print(f"P(B) = {P_B:.4f}")
 
-# 3. Yes
-# 4. Verificacion por formula
+
+# In[8]:
+
+
+# 3 
+print("Punto 3 ->")
+N_woman_and_break =  tabla.iloc[0]['woman']
+N = total_breakfast.sum()
+P_notM_and_break = prob(N_woman_and_break,N)
+print(f"P(M' and B) = {P_notM_and_break:.4f}")
+
+
+# In[9]:
+
+
+# 4 (Malo: Debe dar: 465/595)
+print("Punto 4 ->")
+N_woman_or_break =  total_sex['woman'] + total_breakfast[0] - tabla.iloc[0]['woman']
+N = total_breakfast.sum()
+P_notM_or_break = prob(N_woman_or_break,N)
+print(f"P(M' or B) = {P_notM_or_break:.4f}")
+
+
+# In[10]:
+
+
+# 5
+print("Punto 5 ->")
+N_break_or_man =  total_sex['man'] + total_breakfast[0] - tabla.iloc[0]['man'] # Mirar si se puede hacer de otra forma
+                                                                               # Definicion
+N = total_sex.sum()
+P_B_dado_M = prob(N_break_or_man,N)
+print(f"P(B or M) = {P_B_dado_M:.4f}")
+
+
+# In[11]:
+
+
+# 6 (165/595)
+print("Punto 6 ->")
+#N_break_and_woman =  tabla.iloc[0]['woman']
+#N_breakfast = total_breakfast[0]
+#P_M_dado_B = prob(N_break_and_woman,N_breakfast)
+#print(f"P(M|B) = {P_M_dado_B:.4f}")
+
+
+# In[12]:
+
+
+# 7
+print("Punto 7 ->")
+#N_break_and_woman =  tabla.iloc[0]['woman']
+#N_breakfast = total_breakfast[0]
+#P_M_dado_B = prob(N_break_and_woman,N_breakfast)
+#print(f"P(M|B) = {P_M_dado_B:.4f}")
+
+
+# In[13]:
+
+
+# 8
+print("Punto 8 ->")
+#N_break_and_woman =  tabla.iloc[0]['woman']
+#N_breakfast = total_breakfast[0]
+#P_M_dado_B = prob(N_break_and_woman,N_breakfast)
+#print(f"P(M|B) = {P_M_dado_B:.4f}")
+
+
+# In[14]:
+
+
+# 9. Yes
+print("Punto 3 -> Si")
+
+# 10. Verificacion por formula
+print("Punto 4 -> Se verifica por formula")
 
 
 # #### Ejemplo 2
@@ -102,8 +199,8 @@ print(P_M_dado_B)
 # Si se elije un encuestado al azar:
 # 1. Si la persona seleccionada es un hombre, cual es la probabilidad de que esta responda "Practicamente segura"
 # 2. Si la persona seleccionada responde "Alguna posibilidad pero probablemente no" cual es la probabilidad de que la persona sea mujer
-# 3. Encuentre $P("Gran probabilidad" | Mujer)$
-# 4. Encuentre $P("Gran probabilidad")$
+# 3. Encuentre $P(Gran\;probabilidad | Mujer)$
+# 4. Encuentre $P(Gran\;probabilidad)$
 # 5. Los eventos "Gran probabilidad" y "Mujer" son eventos independientes
 # 
 # * **Solución**
@@ -134,11 +231,11 @@ print(P_M_dado_B)
 # 
 #    $$P(R_5|H) = \frac{P(R_5\;and\;H)}{P(H)} = \frac{\frac{597}{4826}}{\frac{2459}{4826}}$$
 # 
-#    $$P(R_5|H) = \frac{597}{2459} = 0.2459$$
+#    $$P(R_5|H) = \frac{597}{2459} = 0.2428$$
 # 
 #    Otra forma de hallar lo anterior seria aplicando la definición de probabilidad condicional:
 # 
-#    $$P(R_5|H) = \frac{N(R_5\;and\;H)}{N(H)} = \frac{597}{2459} = 0.2459$$ 
+#    $$P(R_5|H) = \frac{N(R_5\;and\;H)}{N(H)} = \frac{597}{2459} = 0.2428$$ 
 # 
 #    Como se puede ver en el procedimiento anterior, los resultados coinciden.
 # 
@@ -155,22 +252,105 @@ print(P_M_dado_B)
 # 
 #    $$P(R_4|M) = \frac{N(R_4\;and\;M)}{N(M)} = \frac{663}{2367} = 0.2801$$
 #    
-# 4. ¿Los eventos D y A son independientes?
+# 4. $P(R_4) = ?$
+# 
+#    En este caso tenemos:
+# 
+#    $$P(R_4) = \frac{N(R_4)}{N} = \frac{1421}{4826} = 0.2944$$
+# 
+# 5. Para responder si $R_4$ y $M$ son eventos independientes se debe cumplir que $P(R_4|M) = P(R_4)$ sin embargo, al analizar las respuestas de los puntos 3 y 4 vemos que $P(R_4|M) \ne P(R_4)$ por lo que estos eventos **No son independientes**
+# 
 #    
 #    
 
-# ## Generalización de la probabilidad total
-# 
-# ```{admonition} Ley de la probabilidad total
-# Dados los eventos $B_1,\;,B_2,\;,B_3,\;,...,B_k$ los cuales son mutuamente exclusivos con $P(B_1)+P(B_2)+P(B_3)+...+P(B_k) = 1$, entonces para cualquier evento $E$:
-# 
-# $$
-# P(E)  = P(E \bigcap B_1) + P(E \bigcap B_2) + ... + P(E \bigcap B_k)
-# P(E)  = P(E1|B_1)P(B_1) + P(E|B_2)P(B_2) + ... + P(E|B_k)P(B_k)
-# $$
-# ```
+# In[15]:
 
-# ### Ejemplos
+
+# Datos
+data = {
+  "respuestas": np.array([1, 2, 3, 4, 5], dtype='str'),
+  "mujeres": np.array([96, 426, 696, 663, 486]),
+  "hombres": np.array([98, 286, 720, 758, 597])
+}
+
+print("--- Tabla --- ")
+tabla = pd.DataFrame(data)
+print(tabla)
+
+
+# In[16]:
+
+
+# Totales
+total_sexo = tabla[tabla.columns[1:3]].sum()
+total_sexo
+
+
+# In[17]:
+
+
+# Totales
+total_respuestas = tabla.sum(axis = "columns", numeric_only = True)
+total_respuestas
+
+
+# In[18]:
+
+
+N_R5_and_H = tabla.iloc[4]['hombres']
+N_H = total_sexo[1]
+P_R5_dado_H = prob(N_R5_and_H, N_H)
+print(f"P(R5|H) = {P_R5_dado_H:0.4f}")
+
+
+# In[19]:
+
+
+N_M_and_R2 = tabla.iloc[1]['mujeres']
+N_R2 = total_respuestas[1]
+P_M_dado_R2 = prob(N_M_and_R2, N_R2)
+print(f"P(M|R2) = {P_M_dado_R2:0.4f}")
+
+
+# In[20]:
+
+
+N_R4_and_M = tabla.iloc[3]['mujeres']
+N_M = total_sexo[0]
+P_R4_dado_M = prob(N_R4_and_M, N_M)
+print(f"P(R4|M) = {P_R4_dado_M:0.4f}")
+
+
+# In[21]:
+
+
+N_R4 = total_respuestas[3]
+N = total_sexo.sum()
+P_R4 = prob(N_R4, N)
+print(f"P(R4) = {P_R4:0.4f}")
+
+
+# #### Ejemplo 3
+# 
+# La siguiente tabla relaciona los pesos y las alturas de un grupo de personas que participan en un estudio de observación.
+# 
+# |Peso|Alto|Medio|Bajo|
+# |---|---|---|---|
+# |Obeso|18|24|14|
+# |Normal|20|51|28|
+# |Bajo peso|12|25|9|
+# 
+# Se pide:
+# 1. Calcule el total de cada fila y columna
+# 2. Calcule la probabilidad de que una persona elegida al azar de este grupo sea alta.
+# 3. Calcule la probabilidad de que una persona elegida al azar de este grupo sea obesa y alta.
+# 4. Calcule la probabilidad de que una persona elegida al azar de este grupo sea alta dado que es obesa
+# 5. Calcule la probabilidad de que una persona elegida al azar de este grupo sea obesa, dado que es alta.
+# 6. Calcule la probabilidad de que una persona elegida al azar de este grupo sea alta y de bajo peso.
+# 7. ¿Los eventos obeso y alto son independientes?
+# 
+
+# pendiente...
 
 # # Referencias
 
